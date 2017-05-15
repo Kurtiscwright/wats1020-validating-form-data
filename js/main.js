6e2d6f68d5e85dac2159c1fce119e8e7cbc03e2c
Kurtis Wright <Kurtiscwright@gmail.com>
2017-05-14 22:30:52 -0700

    Finished With Working Validation Fields

--- js/main.js
+++ js/main.js
@@ -6,3 +6,43 @@
 // 3. Connect the validation object to an event handler tied to the submit button.
 
 // Refer to the `index.html` file for the validation rules that must be enforced.
+$(document).on("ready", function() {
+
+    $("#order-form").validate({
+      submitHandler: function(form) {
+        form.submit();
+      },
+
+      rules: {
+        "your-name": {
+          required: true,
+          maxlength: 128,
+          lettersonly: true
+        },
+        "your-state": {
+          required: true,
+          maxlength: 2
+        },
+        "your-zip": {
+          required: true,
+          maxlength: 5,
+          digits: true
+        },
+        "card-holder-name": {
+          required: true,
+          maxlength: 128
+        },
+        "card-number": {
+          required: true,
+          creditcard: true
+        },
+        "cvv": {
+          required: true,
+          maxlength: 3,
+          digits: true
+        },
+
+      }
+    });
+    $("label span.glyphicon").tooltip();
+});
